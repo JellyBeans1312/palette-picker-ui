9599443374d91fc671a89867d002234cff58e4b3
Eric O'Neill <oneill.eric23@gmail.com>
2019-10-11 17:04:07 -0600

    Finalize merge conflict resolution

--- src/containers/ColorContainer/ColorContainer.js
+++ src/containers/ColorContainer/ColorContainer.js
@@ -4,15 +4,9 @@ import ColorRhombus from '../../components/ColorRhombus/ColorRhombus';
 import './ColorContainer.css';
 
 const ColorContainer = ({ generateNewColors, colors }) => {
-<<<<<<< HEAD
   const colorRhombi = colors.map(color => {
     console.log('COLOR', color)
     return <ColorRhombus color={color.color}/>
-=======
-  console.log('COLORS', colors)
-  const colorRhombi = colors.map(color => {
-    return <ColorRhombus hexCode={color.color}/>
->>>>>>> 19c4aafa53d87e7ea8f41b9065ebec0ea14c0235
   });
 
   return (
--- src/reducers/colorsReducer.js
+++ src/reducers/colorsReducer.js
@@ -1,6 +1,5 @@
 export const colorsReducer = (state = [], action) => {
   switch(action.type) {
-<<<<<<< HEAD
     case 'SAVE_COLOR':
       if(state.length === 5) {
         return action.color
@@ -16,12 +15,7 @@ export const colorsReducer = (state = [], action) => {
           return colorObj
         }
       })
-      console.log("UPDATED COLORS", updatedColors)
       return updatedColors
-=======
-    case 'SAVE_COLORS':
-      return action.colors;
->>>>>>> 19c4aafa53d87e7ea8f41b9065ebec0ea14c0235
     default: 
       return state;
   }
