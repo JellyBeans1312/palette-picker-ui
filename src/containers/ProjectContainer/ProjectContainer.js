d491109794bce0935045a185ca9fb4424391a3c7
Eric O'Neill <oneill.eric23@gmail.com>
2019-10-15 14:53:10 -0600

    Add PropTypes to ProjectContainer

--- src/containers/ProjectContainer/ProjectContainer.js
+++ src/containers/ProjectContainer/ProjectContainer.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './ProjectContainer.css';
-import Project from '../Project/Project'
-import { connect } from 'react-redux'
-
+import Project from '../Project/Project';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 const ProjectContainer = ({ allProjects, allPalettes }) =>
  {
@@ -22,6 +22,11 @@ const ProjectContainer = ({ allProjects, allPalettes }) =>
 const mapStateToProps = state => ({
   allProjects: state.allProjects,
   allPalettes: state.allPalettes
-})
+});
 
 export default connect(mapStateToProps)(ProjectContainer);
+
+ProjectContainer.propTypes = {
+  allProjects: PropTypes.array,
+  allPalettes: PropTypes.array
+}
