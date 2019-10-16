7c9ac22185f02092a1ce2256ffba688e0927125c
Eric O'Neill <oneill.eric23@gmail.com>
2019-10-16 16:35:23 -0600

    Finalize testing for getAllProjects

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -1,4 +1,4 @@
-import { createProject } from './apiCalls';
+import { createProject, getAllProjects } from './apiCalls';
 
 describe('createProject', () => {
   let mockResponse, mockRequest, mockNewProject;
@@ -53,5 +53,54 @@ describe('createProject', () => {
     });
 
     expect(createProject(mockNewProject)).rejects.toEqual({ message: 'There was an issue creating your project.' })
+  });
+});
+
+describe('getAllProjects', () => {
+  let mockResponse;
+
+  beforeEach(() => {
+    mockResponse = [
+      {
+        id: 1
+      }
+    ]
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      })
+    });
+  });
+
+  it('should call fetch with the correct url', () => {
+    getAllProjects();
+
+    expect(window.fetch).toHaveBeenCalledWith('https://palette-picker-be-eo-am.herokuapp.com/api/v1/projects')
+  });
+
+  it('should return an array of all projects from the database', () => {
+    getAllProjects()
+    .then(results => expect(results).toEqual(mockResponse))
+  });
+
+  it('should throw an error if the response.status is not ok', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      });
+    });
+
+    expect(getAllProjects()).rejects.toEqual(Error('There was an error retrieving your projects. Please try again.'))
+  });
+
+  it('should throw an error if the promise rejects', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject({
+        message: 'There was an error retrieving your projects. Please try again.'
+      })
+    });
+
+    expect(getAllProjects()).rejects.toEqual({ message: 'There was an error retrieving your projects. Please try again.' })
   });
 });
