929ab5a8410e5e61833cf9bc953265ced5c3756c
Aidan McKay <aidanmckay2000@gmail.com>
2019-10-16 14:11:50 -0600

    add styling and snapshot test to App

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,27 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import { App } from './App';
+import { shallow } from 'enzyme';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+describe('App', () => {
+  let wrapper;
+  const mockColors = [{}]
+  const mockProject = {}
+  const mockAllProjects=[{}]
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <App
+        colors={mockColors}
+        project={mockProject}
+        allProjects={mockAllProjects}
+        addAllPalettes={jest.fn()}
+        addAllProjects={jest.fn()}
+        addPalette={jest.fn()}
+        addProject={jest.fn()}
+      />
+    )
+  })
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
