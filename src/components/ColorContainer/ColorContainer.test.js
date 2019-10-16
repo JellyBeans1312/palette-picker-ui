b89a204bc4089df105cb4ca2a34c58906ef98ec1
Eric O'Neill <oneill.eric23@gmail.com>
2019-10-15 19:25:14 -0600

    Update package.json and test ColorContainer

--- src/components/ColorContainer/ColorContainer.test.js
+++ src/components/ColorContainer/ColorContainer.test.js
@@ -3,21 +3,41 @@ import { shallow } from 'enzyme';
 import { ColorContainer }  from './ColorContainer';
 
 describe('ColorContainer', () => {
-  it('should match the snapshot with all data passed in correctly', () => {
-    const wrapper = shallow(<ColorContainer 
+  let wrapper, mockUpdatePalette, mockGenerateNewColors;
+
+  beforeEach(() => {
+    mockUpdatePalette = jest.fn();
+    mockGenerateNewColors = jest.fn();
+    wrapper = shallow(<ColorContainer
       generateNewColors={jest.fn()}
       colors={
         [
-          {color: "#493c4d", isLocked: false},
+          { color: "#493c4d", isLocked: false },
           { color: "#567c4d", isLocked: false },
           { color: "#493c4f", isLocked: false },
           { color: "#493c67", isLocked: false },
           { color: "#FFFFFF", isLocked: false }
         ]
       }
-      updatePalette={jest.fn()}
+      generateNewColors={mockGenerateNewColors}
+      updatePalette={mockUpdatePalette}
     />);
+  });
+
+  it('should match the snapshot with all data passed in correctly', () => {
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call generateNewColors when the Generate a new palette button is clicked', () => {
+    wrapper.find('button').at(0).simulate('click');
+
+    expect(mockGenerateNewColors).toHaveBeenCalled();
+  });
+
+  it('should call updatePalette when the Update Palette button is clicked', () => {
+    wrapper.find('button').at(1).simulate('click');
+
+    expect(mockUpdatePalette).toHaveBeenCalled();
   });
 });
