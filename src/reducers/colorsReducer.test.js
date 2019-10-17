375a2b6e65cd9d056db7567eeded7420410b80fc
Eric O'Neill <oneill.eric23@gmail.com>
2019-10-16 18:24:26 -0600

    Finalize test for colorsReducer

--- src/reducers/colorsReducer.test.js
+++ src/reducers/colorsReducer.test.js
@@ -22,5 +22,74 @@ describe('colorsReducer', () => {
     expect(result).toEqual(expected);
   });
 
-  it('should')
+  it('should add a color to state if state.length < 5', () => {
+    let expected = [{}, {}, {}, {}, {color: '#fff', isLocked: false}];
+
+    let mockAction = {
+      type: 'SAVE_COLOR',
+      color: { color: '#fff', isLocked: false }
+    }
+    let result = colorsReducer([{}, {}, {}, {}], mockAction);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should lock a color if the color matches a color in state', () => {
+    let expected = [{ color: '#fff', isLocked: false }, { color: '#aaa', isLocked: false }, { color: '#ddddd', isLocked: false }, { color: '#fc54ad', isLocked: false }, { color: '#000', isLocked: true }];
+
+    let mockAction = {
+      type: 'LOCK_COLOR',
+      color: { color: '#000', isLocked: false }
+    }
+  
+    let result = colorsReducer([{ color: '#fff', isLocked: false }, { color: '#aaa', isLocked: false }, { color: '#ddddd', isLocked: false }, { color: '#fc54ad', isLocked: false }, { color: '#000', isLocked: true }], mockAction);
+  
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a palette', () => {
+    let expected = [
+      {
+        color: '#000',
+        isLocked: true,
+        id: 5
+      },
+      {
+        color: '#aaa',
+        isLocked: true,
+        id: 5
+      },
+      {
+        color: '#bbb',
+        isLocked: true,
+        id: 5
+      },
+      {
+        color: '#ccc',
+        isLocked: true,
+        id: 5
+      },
+      {
+        color: '#fff',
+        isLocked: true,
+        id: 5
+      }
+    ];
+
+    let mockAction = {
+      type: 'ADD_PALETTE',
+      palette: {
+        id: 5,
+        color_one: '#000',
+        color_two: '#aaa',
+        color_three: '#bbb',
+        color_four: '#ccc',
+        color_five: '#fff'
+      }
+    }
+
+    let result = colorsReducer([], mockAction);
+
+    expect(result).toEqual(expected);
+  });
 });
