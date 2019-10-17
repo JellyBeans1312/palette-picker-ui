5cde605c442e8d08d84bd560ea3bfe84f5631151
Eric O'Neill <oneill.eric23@gmail.com>
2019-10-16 19:44:06 -0600

    Finalize reducer testing

--- src/reducers/projectReducer.test.js
+++ src/reducers/projectReducer.test.js
@@ -38,6 +38,95 @@ describe('projectReducer', () => {
 
 describe('allProjectsReducer', () => {
   it('should return the default state', () => {
+    let expected = [];
 
+    let result = allProjectsReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return allprojects if ADD_ALL_PROJECTS is called', () => {
+    let allProjects = [
+      {
+        project_name: 'test',
+        id: 1
+      },
+      {
+        project_name: 'test2',
+        id: 2
+      }
+    ];
+
+    let expected = [
+      {
+        project_name: 'test',
+        id: 1
+      },
+      {
+        project_name: 'test2',
+        id: 2
+      }
+    ]
+
+    let mockAction = {
+      type: 'ADD_ALL_PROJECTS',
+      allProjects
+    }
+
+    let result = allProjectsReducer([], mockAction);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should delete a project from state if REMOVE_PROJECT is called', () => {
+    let mockState = [
+      {
+        project_name: 'test',
+        id: 1
+      },
+      {
+        project_name: 'test2',
+        id: 2
+      }
+    ];
+
+    let expected = [
+      {
+        project_name: 'test',
+        id: 1
+      }
+    ];
+
+    let mockAction = {
+      type: 'REMOVE_PROJECT',
+      id: 2
+    }
+
+    let result = allProjectsReducer(mockState, mockAction);
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('editProjectNameReducer', () => {
+  it('should return the default state', () => {
+    let expected = false;
+
+    let result = editProjectNameReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the action.status if UPDATE_PROJECT_NAME is called', () => {
+    let expected = true;
+
+    let mockAction = {
+      type: 'UPDATE_PROJECT_NAME',
+      status: true
+    }
+
+    let result = editProjectNameReducer(false, mockAction);
+
+    expect(result).toEqual(expected);
   });
 });
